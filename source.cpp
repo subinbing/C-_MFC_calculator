030de545e62b104857f66d07720236d014ce1ece
박수빈 <87634136+subinbing@users.noreply.github.com>
2022-06-20 03:21:27 +0900

    Add files via upload

---
+++ source.cpp
@@ -0,0 +1,33 @@
+﻿// 수식 Button 부분 코드
+
+void CMFCApplication3Dlg::OnBnClickedButton1()
+{
+	int a = GetDlgItemInt(IDC_EDIT1);
+	int b = GetDlgItemInt(IDC_EDIT2);
+	SetDlgItemInt(IDC_EDIT3, a + b);
+}
+
+
+void CMFCApplication3Dlg::OnBnClickedButton2()
+{
+	int a = GetDlgItemInt(IDC_EDIT1);
+	int b = GetDlgItemInt(IDC_EDIT2);
+	SetDlgItemInt(IDC_EDIT3, a - b);
+}
+
+
+void CMFCApplication3Dlg::OnBnClickedButton3()
+{
+	int a = GetDlgItemInt(IDC_EDIT1);
+	int b = GetDlgItemInt(IDC_EDIT2);
+	SetDlgItemInt(IDC_EDIT3, a * b);
+}
+
+
+void CMFCApplication3Dlg::OnBnClickedButton4()
+{
+	int a = GetDlgItemInt(IDC_EDIT1);
+	int b = GetDlgItemInt(IDC_EDIT2);
+	SetDlgItemInt(IDC_EDIT3, a / b);
+}
+​
